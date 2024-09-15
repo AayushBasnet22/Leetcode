062467135844c8f428fdb1136b23b8550b14dbd0
Aayush Basnet <83830996+AayushBasnet22@users.noreply.github.com>
2024-09-15 23:31:50 +0545

    Error file

--- LengthOfLongestSubstring.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
-        
-if __name__ == '__main__':
-    S = Solution()
-    print(S.lengthOfLongestSubstring('pwwkew'))
